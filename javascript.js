ed30efd3eaa109c5a131b674918f3fdb34bff5a8
codedlovely <submerginglife@gmail.com>
2022-04-16 13:50:56 +0900

    Change grid divs color when cursor hovers over

--- javascript.js
+++ javascript.js
@@ -3,18 +3,30 @@ const containerWidth = "540";
 const containerHeight = "540";
 const divWidth = "18";
 const numberDivs = containerWidth/divWidth * containerHeight/divWidth;
-//console.log(`divs = ${numberDivs}`);
 
 // fill container with divs
 for (let i=0; i<numberDivs; i++) {
     addDiv();
 }
 
+// change div background color when cursor hovers over
+let divGrid = document.getElementById('container');
+divGrid.childNodes.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'pink';
+    });
+});
+
 //----------
 // add one div to container
 //
 function addDiv() {
+    // create div
     const newDiv = document.createElement('div');
+    // add class 'grid' to div
+    newDiv.classList.add('grid');
+    // container is where div will be added to
     const currentDiv = document.getElementById('container');
+    // add div to container
     currentDiv.appendChild(newDiv);
 }
