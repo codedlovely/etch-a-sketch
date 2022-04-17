361452d22857de932c78c318af03e92de6358a94
codedlovely <submerginglife@gmail.com>
2022-04-17 10:23:55 +0900

    Add a switch to select rainbow colors

--- javascript.js
+++ javascript.js
@@ -3,6 +3,11 @@ const containerWidth = "540";
 const containerHeight = "540";
 const divWidth = "18";
 const numberDivs = (containerWidth/divWidth) ** 2;
+const alertMessage = "Please input an integer between 20 and 100"
+const colorSelect = "pink";
+const colorNames = ['red', 'orange', 'yellow', 'limegreen', 'turquoise', 'blue', 'blueviolet'];
+
+let colorSwitch = 0;
 
 // set container width & height
 const divContainer = document.getElementById('container');
@@ -17,8 +22,16 @@ for (let i=0; i<numberDivs; i++) {
 // change div color
 divColorChange();
 
+// switch between single color and rainbow
+let switchButton = document.getElementById('toggle-rainbow');
+switchButton.addEventListener('click', () => {
+
+    // set color selection
+    colorSwitch = switchButton.checked;
+})
+
 // set button to do these things:
-//  1. ask user how many squares per side
+//  1. ask user how many squares per side they want
 //  2. clear current grid
 //  3. calculate div width
 //  4. create a new grid
@@ -26,9 +39,17 @@ let gridButton = document.getElementById('refresh');
 gridButton.addEventListener('click', () => {
 
     // show prompt
-    let numGrid = window.prompt('How many squares per side do you want to put?');
+    let numGrid = window.prompt('How many squares per side do you want to create? (20 to 100)');
+    if (numGrid == null || numGrid == 0) {
+        return;
+    }
     if (numGrid > 100 || numGrid < 20) {
-        alert('Please input a number between 20 and 100');
+        alert(alertMessage);
+        return;
+    }
+    let tempnum = Number(numGrid);
+    if (Number.isInteger(tempnum) == false) {
+        alert(alertMessage);
         return;
     }
     
@@ -40,7 +61,7 @@ gridButton.addEventListener('click', () => {
     let newWidth = containerWidth / numGrid - 2;
     
     // set div width & heigth
-    for (let i=0; i<numGrid*numGrid; i++) {
+    for (let i=0; i<numGrid**2; i++) {
         addDiv(newWidth);
     }
 
@@ -53,7 +74,6 @@ gridButton.addEventListener('click', () => {
 function addDiv(newwidth) {
     
     if (newwidth == null || newwidth == 0) {
-        console.log(newwidth);
         newwidth = divWidth;
     }
 
@@ -90,8 +110,35 @@ function divColorChange() {
     let divGrid = document.getElementById('container');
     divGrid.childNodes.forEach((div) => {
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'pink';
+            div.style.backgroundColor = colorSelect;
+            if (colorSwitch) {
+                // rainbow
+                div.style.backgroundColor = getRandomColorname();
+            } else {
+                div.style.backgroundColor = colorSelect;
+            }
         });
     });
 }
-    
+
+//----------
+// return one of rainbow colornames
+//
+function getRandomColorname () {
+
+    // get a random number
+    let num = getRandomInt(0, 6);
+
+    // return colorname
+    return colorNames[num];
+
+}
+
+//----------
+// return random number between x and y, include x and y
+//
+function getRandomInt (x, y) {
+    x = Math.ceil(x);
+    y = Math.floor(y);
+    return Math.floor(Math.random() * (y - x + 1) + x);
+}
